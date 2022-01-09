867e9aaac25be19375890d2f603597972641891d
Jarob <jarobheffner@gmail.com>
2022-01-08 18:14:22 -0700

    made to send time and speed

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     ser.reset_input_buffer()
     while True:
-        ser.write(b"Hello from Raspberry Pi!\n")
+        ser.write(b"HIGH\n TIME\n")
         line = ser.readline().decode('utf-8').rstrip()
         print(line)
         time.sleep(1)
